89e992fd4a2a8d144c91b81d4f8364a886a2b471
agakansha9826 <146553826+agakansha9826@users.noreply.github.com>
2023-10-07 18:12:09 +0530

    reminder.php

---
+++ reminder.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reminder</title>
+    <link rel="stylesheet" href="remind.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto&family=Young+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div class="header">
+    <h1 id="remind">Reminder!!!</h1>
+    </div>
+    
+    <div class="table">
+        <section class="table_header">
+            <h1><b>Order Details</b></h1>
+        </section>
+        <section class="table_body"> 
+            <table>
+                <thead>
+                    <tr>
+                        <th>Book</th>
+                        <th>Issue Date</th>
+                        <th>Due Date</th>
+                        <th>Penalty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <b>
+                   <tr>
+                    <td>ABC</td>
+                    <td>12/12/12</td>
+                    <td>12/06/13</td>
+                    <td>0</td>
+                   </tr>
+                   <tr>
+                    <td>XYZ</td>
+                    <td>15/08/23</td>
+                    <td>14/02/24</td>
+                    <td>4</td>
+                   </tr>
+                   <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                   </tr>
+                   <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                   </tr>
+                    </b>   
+                </tbody>
+            </table>
+        </section>
+    </div>
+    <div class="calender">
+        <h1 id="reminder_calender">Calender</h1>
+        <iframe src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FKolkata&showTitle=0&showPrint=0&showTabs=1&showTz=1&src=MTcxOTA4OGM3MWMzNzZhNDA4NzMwZTJiZmRhN2U0MGUwNWUxZDdiZDQ1ZjliYzg3NDE3MWFlYTdlYTAyNTZhOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23C0CA33&color=%230B8043" style="border:solid 1px #777" width="400" height="400" ></iframe>
+    </div>
+    
+</body>
+</html>
