db7891fe7609e8b15e2925f23c58aeed5e8cb5f9
agakansha9826 <146553826+agakansha9826@users.noreply.github.com>
2023-10-07 18:13:49 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="navdiv">
+                <div class="logo"><a href="#">IET Library</a></div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Add Books</a></li>
+                    <li><a href="#">Issue</a></li>
+                    <li><a href="#">About us</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div class="body">
+        <div class="table">
+            <table>
+                <tr>
+                    <td><button>First Year</button></td>
+                </tr>
+                <tr>
+                    <td><button>Second Year</button></td>
+                </tr>
+                <tr>
+                    <td><button>Third Year</button></td>
+                </tr>
+                <tr>
+                    <td><button>Fourth Year</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+   
+</body>
+</html>
